86cfaaa70d0e58a49af8f8324afffe0434af5a5e
Abdullah Alger <abdullahalger@me.com>
2016-05-21 20:49:36 +0200

    .

---
+++ 01_getting-started/02_numerical-systems/02_binary/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// print with formatting
+	fmt.Printf("%d - %b \n", 43, 43)
+}
---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
+	"github.com/ajalger/02_package/stringUtil"
+)
+
+func main()  {
+	fmt.Println(stringutil.MyName)
+	fmt.Println(stringutil.Reverse("qwerty"))
+}
