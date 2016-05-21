d22500557b0cc6b364d40b8541f81b94ea709187
Abdullah Alger <abdullahalger@me.com>
2016-05-21 20:53:39 +0200

    .

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
-	"github.com/ajalger/02_package/stringUtil"
+	"github.com/ajalger/learn-go/master/02_package/stringutil"
+
 )
 
 func main()  {
 	fmt.Println(stringutil.MyName)
-	fmt.Println(stringutil.Reverse("qwerty"))
 }
