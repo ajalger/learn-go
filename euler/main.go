c703c7c10b6d90d0b55cde77440190749c5816dd
Abdullah Alger <abdullahalger@me.com>
2016-05-30 19:09:15 +0200

    euler 4

---
+++ euler/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"strconv"
+	"fmt"
+)
+
+func main() {
+	p := getProducts()
+	fmt.Println(p)
+}
+
+func getProducts() int {
+	var arr []int
+	for i := 100; i < 1000; i++ {
+		for j := i + 1; j < 1000; j++ {
+			m := multiplyToString(i, j)
+			if palindrome(m) {
+				m, _ := strconv.Atoi(m)
+				arr = append(arr, m)
+			}
+		}
+	}
+	return largestNumber(arr)
+}
+
+func largestNumber(arr []int) int {
+	var largest int
+	for _, v := range arr {
+		if largest < v {
+			largest = v
+		}
+	}
+	return largest
+}
+
+func multiplyToString(num1 int, num2 int) string {
+	total := num1 * num2
+	return strconv.Itoa(total)
+}
+
+func palindrome(num string) bool {
+	var length = len(num)
+	for i := 0; i < (length / 2); i++ {
+		if num[i] != num[length - 1 - i] {
+			return false
+		}
+	}
+	return true
+}
