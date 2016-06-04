839854c1073073a84ff8044f720c3f5f44f077bf
Abdullah Alger <abdullahalger@me.com>
2016-06-04 08:35:11 +0200

    .

--- 18_slice/01_int-slice/main.go
+++ 18_slice/01_int-slice/main.go
@@ -7,4 +7,8 @@ func main() {
 	mySlice := []int{1, 3, 5, 7, 9, 11}
 	fmt.Printf("%T\n", mySlice)
 	fmt.Println(mySlice)
+
+	newSlice := make([]int, 50, 100)
+	fmt.Println(newSlice)
+	fmt.Println(cap(newSlice))
 }
