80d57847665bd9ad9a0244d7aa7836448d537e54
Abdullah Alger <abdullahalger@me.com>
2016-06-09 17:49:41 +0200

    .

--- 19_map/09_deleting-entry/main.go
+++ 19_map/09_deleting-entry/main.go
@@ -12,6 +12,19 @@ func main() {
 	}
 
 	fmt.Println(myGreeting)
+	//delete(myGreeting, "two")
+	//delete(myGreeting, "three")
+	myGreeting["four"] = "What Up!"
+	myGreeting["one"] = "Asalamu Alaykum"
+	myGreeting["five"] = "Domo Arigato"
+
+	fmt.Println(len(myGreeting))
+
+	delete(myGreeting, "zero")
+	delete(myGreeting, "one")
 	delete(myGreeting, "two")
+
+	fmt.Println(len(myGreeting))
+
 	fmt.Println(myGreeting)
 }
